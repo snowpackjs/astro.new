d4eb560fa01d2a2fbd1e37d6d6c3727d8bb979d5
Luiz Ferraz <luiz@lferraz.com>
2025-01-18 10:22:37 -0300

    Update idx-template.nix

--- .idx-templates/latest/idx-template.nix
+++ .idx-templates/latest/idx-template.nix
@@ -9,12 +9,11 @@
   ];
 
   bootstrap = ''
-    mkdir "$out"
-    echo y | npm create astro@latest "$out" -- --template ${astroTemplate} --git --no-install --skip-houston --yes
+    mkdir -p "$out"
+    cd "$out"
+    echo y | npm create astro@latest . -- --template ${astroTemplate} --git --no-install --skip-houston --yes
 
-    mkdir -p "$out"/.idx
-    cp ${./dev.nix} "$out/.idx/dev.nix"
-
-    ( cd "$out" && npm i --package-lock-only --ignore-scripts )
+    mkdir -p .idx
+    cp ${./dev.nix} ".idx/dev.nix"
   '';
 }

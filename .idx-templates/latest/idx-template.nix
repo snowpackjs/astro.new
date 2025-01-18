f4df3dbbbd284fd027e9ea5f532aa2bbd45dec20
Luiz Ferraz <luiz@lferraz.com>
2025-01-18 11:56:12 -0300

    Update idx-template.nix

--- .idx-templates/latest/idx-template.nix
+++ .idx-templates/latest/idx-template.nix
@@ -14,17 +14,18 @@
   set -x
   logfile="$PWD/create.log"
   {
-    mkdir -p "$out"
-    cd "$out"
     env
     which npm
     which node
-    echo y | timeout -k 15s 15s npm create astro@latest . -- --template ${astroTemplate} --git --no-install --skip-houston --yes | cat || true
+    echo y | timeout -k 15s 15s npm create astro@latest project -- --template ${astroTemplate} --git --no-install --skip-houston --yes | cat || true
 
+    mv project "$out"
     mkdir -p .idx
     cp ${./dev.nix} ".idx/dev.nix"
   } 2>&1 | tee "$logfile"
 
   mv "$logfile" "$out/create.log"
+  cd "$out"
+  exit 0
   '';
 }

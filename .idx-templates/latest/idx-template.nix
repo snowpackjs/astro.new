a32640831a94638c7758b95cb132e8b1bec115ac
Luiz Ferraz <luiz@lferraz.com>
2025-01-18 10:26:49 -0300

    Update idx-template.nix

--- .idx-templates/latest/idx-template.nix
+++ .idx-templates/latest/idx-template.nix
@@ -11,7 +11,7 @@
   bootstrap = ''
     mkdir -p "$out"
     cd "$out"
-    echo y | npm create astro@latest . -- --template ${astroTemplate} --git --no-install --skip-houston --yes
+    echo y | timeout -k 30s 30s npm create astro@latest . -- --template ${astroTemplate} --git --no-install --skip-houston --yes
 
     mkdir -p .idx
     cp ${./dev.nix} ".idx/dev.nix"
